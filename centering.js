39d47146c2021916699f2c21eb2f638833960e1a
Caymen2299 <caymencarter@gmail.com>
2019-09-18 12:39:14 -0400

    major changes, full redesign, not centered yet

--- centering.js
+++ centering.js
@@ -1,8 +1,9 @@
+/* Toggle between adding and removing the "responsive" class to topnav when the user clicks on the icon */
 function myFunction() {
-    var x = document.getElementById("centered_nav");
-    if (x.className === "rc_nav") {
-        x.className += " responsive";
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
     } else {
-        x.className = "rc_nav";
+      x.className = "topnav";
     }
-}
+  }
